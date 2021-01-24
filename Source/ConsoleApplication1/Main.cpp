7269963ef04f7c9b6618404e2f18c19186d6179d
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2021-01-24 16:25:21 +0100

    Update Main.cpp

--- Source/ConsoleApplication1/Main.cpp
+++ Source/ConsoleApplication1/Main.cpp
@@ -1,17 +1,53 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <iostream>
-#include <tchar.h>
-#include <vector>
-#include "NierHook.hpp"
-#include "Player.hpp"
 #include <thread>
+#include "NierHook.hpp"
 
+//Function used to exit the program
+void ENDPressed(NieRHook* hook) {
+	while (true) {
+		if (GetKeyState(VK_END) & 0x8000) //END button pressed
+		{
+			//Disable cheats before exiting
+			hook->InfiniteDoubleJump(false);
+			hook->IgnoreUpgradeMaterials(false);
+			//Stop hook
+			hook->stop();
+			return;//exit function
+		}
+	}
+}
+
+/*This is a showcase program of the hook
+* As NieR:Automata is a x64 program, you must compile this solution in x64.
+*/
 int main()
 {
 	NieRHook hook;
-	while (true) {
+	//Hook to process
+	while (!hook.isHooked()) {
 		hook.start();
 	}
+
+	//Enable some cheats
+	hook.InfiniteDoubleJump(true);
+	hook.IgnoreUpgradeMaterials(true);
+
+	//Create a thread to exit when the 'END' button is pressed
+	std::thread exitThread(ENDPressed, &hook);
+
+	//Print some values
+	while (hook.isHooked()) {
+		hook.update();//update hook internal values (Position, Health, Level...)
+		std::cout << "X: " << hook.getXPosition() << "  Y: " << hook.getYPosition() << "  Z: " << hook.getYPosition() << std::endl;
+		std::cout << "Health: " << hook.getHealth() << std::endl;
+		std::cout << "Press END to exit..." << std::endl;
+		Sleep(500);
+		system("cls");
+	}
+
+	//Join thread and exit
+	exitThread.join();
 	return (0);
 }
