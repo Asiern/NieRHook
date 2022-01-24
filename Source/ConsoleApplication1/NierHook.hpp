a8f39fa0c07a39a55b0d664b58f44584512a441d
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2022-01-24 08:56:21 +0100

    add: clang-format

--- Source/ConsoleApplication1/NierHook.hpp
+++ Source/ConsoleApplication1/NierHook.hpp
@@ -8,137 +8,133 @@
 class NieRHook;
 class NieRHook
 {
-private:
-	// Attributes
-	DWORD _pID;
-	uintptr_t _baseAddress;
-	uintptr_t _entityAddress;
-	bool _hooked;
-	offsets _offsets;
+  private:
+    // Attributes
+    DWORD _pID;
+    uintptr_t _baseAddress;
+    uintptr_t _entityAddress;
+    bool _hooked;
+    offsets _offsets;
 
-	// Player Attributes
-	int Level;
-	int Health;
-	int MaxHealth;
-	float Xpos;
-	float Ypos;
-	float Zpos;
-	int Funds;
-	int EXP;
-	int version;
+    // Player Attributes
+    int Level;
+    int Health;
+    int MaxHealth;
+    float Xpos;
+    float Ypos;
+    float Zpos;
+    int Funds;
+    int EXP;
+    int version;
 
-	// Methods
-	DWORD _getProcessID(void);
-	uintptr_t _getModuleBaseAddress(DWORD procId, const wchar_t *modName);
-	void _patch(BYTE *destination, BYTE *src, unsigned int size);
-	template <typename T>
-	T readMemory(uintptr_t address);
-	template <typename T>
-	void writeMemory(uintptr_t address, T value);
-	char *readMemoryString(uintptr_t address, int size);
-	void getGameVersion();
+    // Methods
+    DWORD _getProcessID(void);
+    uintptr_t _getModuleBaseAddress(DWORD procId, const wchar_t* modName);
+    void _patch(BYTE* destination, BYTE* src, unsigned int size);
+    template <typename T> T readMemory(uintptr_t address);
+    template <typename T> void writeMemory(uintptr_t address, T value);
+    char* readMemoryString(uintptr_t address, int size);
+    void getGameVersion();
 
-public:
-	NieRHook();
-	~NieRHook();
-	void start(void); // Start hook
-	void stop(void);  // Reset hook
-	bool isHooked(void);
-	void hookStatus(void);
-	int getVersion(void);
+  public:
+    NieRHook();
+    ~NieRHook();
+    void start(void); // Start hook
+    void stop(void);  // Reset hook
+    bool isHooked(void);
+    void hookStatus(void);
+    int getVersion(void);
 
-	DWORD getProcessID(void); // Returns process id
+    DWORD getProcessID(void); // Returns process id
 
-	void update(void); // Update Player Attributes
+    void update(void); // Update Player Attributes
 
-	// Getters
-	int getLevel(void);
-	int getHealth(void);
-	int getMaxHealth(void);
-	int getFunds(void);
-	int getEXP(void);
-	float getXPosition(void);
-	float getYPosition(void);
-	float getZPosition(void);
+    // Getters
+    int getLevel(void);
+    int getHealth(void);
+    int getMaxHealth(void);
+    int getFunds(void);
+    int getEXP(void);
+    float getXPosition(void);
+    float getYPosition(void);
+    float getZPosition(void);
 
-	// Setters
-	void setX(float X);
-	void setY(float Y);
-	void setZ(float Z);
-	void setPosition(float X, float Y, float Z);
-	void setHealth(int health);
-	void setFunds(int funds);
+    // Setters
+    void setX(float X);
+    void setY(float Y);
+    void setZ(float Z);
+    void setPosition(float X, float Y, float Z);
+    void setHealth(int health);
+    void setFunds(int funds);
 
-	// Cheats
-	void NoCLip(bool enabled);
-	void InfiniteDoubleJump(bool enabled);
-	void setGameSpeed(float speed);
-	void NoCooldown(bool enabled);
-	void InfiniteAirDash(bool enabled);
-	void IgnoreUpgradeMaterials(bool enabled);
-	void FreeCamera(bool enabled);
-	void InfiniteItemUsage(bool enabled);
+    // Cheats
+    void NoCLip(bool enabled);
+    void InfiniteDoubleJump(bool enabled);
+    void setGameSpeed(float speed);
+    void NoCooldown(bool enabled);
+    void InfiniteAirDash(bool enabled);
+    void IgnoreUpgradeMaterials(bool enabled);
+    void FreeCamera(bool enabled);
+    void InfiniteItemUsage(bool enabled);
 
-	// Inventory
-	bool addItem(int ID, int number);
-	bool removeItem(int ID);
-	bool addWeapon(int ID, int level);
-	bool removeWeapon(int ID);
+    // Inventory
+    bool addItem(int ID, int number);
+    bool removeItem(int ID);
+    bool addWeapon(int ID, int level);
+    bool removeWeapon(int ID);
 
-	// Misc
-	void setMusicVolume(int value);
-	void setSoundEffectVolume(int value);
-	void setVoiceVolume(int value);
-	void setAudioOutput(int value);
-	void setVoiceChanger(int value);
-	void setBrightness(int value);
-	void setDistance(int value);
-	void setCombatDistance(int value);
-	void setVerticalRotationSpeed(int value);
-	void setHorizontalRotationSpeed(int value);
-	void setHorizontalAutoAdjust(int value);
-	void setVerticalAutoAdjust(int value);
-	void setFreeEnemyTracking(int value);
-	void setZoomSpeed(int value);
-	void setPursuitSpeed(int value);
-	void setLockedEnemyTracking(int value);
-	void setHUDOpacity(float opacity);
-	void setColor(float R, float G, float B);
-	int getMusicVolume();
-	int getSoundEffectVolume();
-	int getVoiceVolume();
-	int getAudioOutput();
-	int getVoiceChanger();
-	int getBrightness();
-	int getDistance();
-	int getCombatDistance();
-	int getVerticalRotationSpeed();
-	int getHorizontalRotationSpeed();
-	int getHorizontalAutoAdjust();
-	int getVerticalAutoAdjust();
-	int getFreeEnemyTracking();
-	int getZoomSpeed();
-	int getPursuitSpeed();
-	int getLockedEnemyTracking();
-	float getHUDOpacity();
+    // Misc
+    void setMusicVolume(int value);
+    void setSoundEffectVolume(int value);
+    void setVoiceVolume(int value);
+    void setAudioOutput(int value);
+    void setVoiceChanger(int value);
+    void setBrightness(int value);
+    void setDistance(int value);
+    void setCombatDistance(int value);
+    void setVerticalRotationSpeed(int value);
+    void setHorizontalRotationSpeed(int value);
+    void setHorizontalAutoAdjust(int value);
+    void setVerticalAutoAdjust(int value);
+    void setFreeEnemyTracking(int value);
+    void setZoomSpeed(int value);
+    void setPursuitSpeed(int value);
+    void setLockedEnemyTracking(int value);
+    void setHUDOpacity(float opacity);
+    void setColor(float R, float G, float B);
+    int getMusicVolume();
+    int getSoundEffectVolume();
+    int getVoiceVolume();
+    int getAudioOutput();
+    int getVoiceChanger();
+    int getBrightness();
+    int getDistance();
+    int getCombatDistance();
+    int getVerticalRotationSpeed();
+    int getHorizontalRotationSpeed();
+    int getHorizontalAutoAdjust();
+    int getVerticalAutoAdjust();
+    int getFreeEnemyTracking();
+    int getZoomSpeed();
+    int getPursuitSpeed();
+    int getLockedEnemyTracking();
+    float getHUDOpacity();
 };
 
-template <typename T>
-inline T NieRHook::readMemory(uintptr_t address)
+template <typename T> inline T NieRHook::readMemory(uintptr_t address)
 {
-	T value;
-	HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, this->_pID);
-	ReadProcessMemory(pHandle, (LPCVOID)(address), &value, sizeof(value), NULL);
-	CloseHandle(pHandle);
-	return value;
+    T value;
+    HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, this->_pID);
+    ReadProcessMemory(pHandle, (LPCVOID)(address), &value, sizeof(value), NULL);
+    CloseHandle(pHandle);
+    return value;
 }
 
-template <typename T>
-inline void NieRHook::writeMemory(uintptr_t address, T value)
+template <typename T> inline void NieRHook::writeMemory(uintptr_t address, T value)
 {
-	HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, this->_pID);
-	WriteProcessMemory(pHandle, (LPVOID)(address), &value, sizeof(value), NULL);
-	CloseHandle(pHandle);
+    HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, this->_pID);
+    WriteProcessMemory(pHandle, (LPVOID)(address), &value, sizeof(value), NULL);
+    CloseHandle(pHandle);
 }
 
 #endif
--- Source/ConsoleApplication1/Offsets.hpp
+++ Source/ConsoleApplication1/Offsets.hpp
@@ -1,67 +1,69 @@
 #include <Windows.h>
 #pragma once
 
-typedef struct {
-	int size;
-	uintptr_t offset;
-	BYTE* enabled;
-	BYTE* disabled;
-}cheat;
+typedef struct
+{
+    int size;
+    uintptr_t offset;
+    BYTE* enabled;
+    BYTE* disabled;
+} cheat;
 
-struct offsets {
-	//Game
-	uintptr_t version;
-	uintptr_t GameSpeed;
+struct offsets
+{
+    // Game
+    uintptr_t version;
+    uintptr_t GameSpeed;
 
-	//Player
-	uintptr_t entity;
-	uintptr_t health;
-	uintptr_t maxHealth;
-	uintptr_t x;
-	uintptr_t y;
-	uintptr_t z;
-	uintptr_t level;
-	uintptr_t exp;
-	uintptr_t funds;
+    // Player
+    uintptr_t entity;
+    uintptr_t health;
+    uintptr_t maxHealth;
+    uintptr_t x;
+    uintptr_t y;
+    uintptr_t z;
+    uintptr_t level;
+    uintptr_t exp;
+    uintptr_t funds;
 
-	//Items
-	uintptr_t items_first;
-	uintptr_t items_last;
+    // Items
+    uintptr_t items_first;
+    uintptr_t items_last;
 
-	//Weapons
-	uintptr_t weapons_first;
-	uintptr_t weapons_last;
+    // Weapons
+    uintptr_t weapons_first;
+    uintptr_t weapons_last;
 
-	//Misc
-	uintptr_t MusicVolume;
-	uintptr_t SoundEffectVolume;
-	uintptr_t VoiceVolume;
-	uintptr_t AudioOutput;
-	uintptr_t VoiceChanger;
-	uintptr_t ScreenBrightness;
-	uintptr_t Distance;
-	uintptr_t CombatDistance;
-	uintptr_t VerticalRotationSpeed;
-	uintptr_t HorizontalRotationSpeed;
-	uintptr_t HorizontalAutoAdjust;
-	uintptr_t VerticalAutoAdjust;
-	uintptr_t FreeEnemyTracking;
-	uintptr_t ZoomSpeed;
-	uintptr_t PursuitSpeed;
-	uintptr_t LockedEnemyTracking;
-	uintptr_t HUDOpacity;
-	uintptr_t R;
-	uintptr_t G;
-	uintptr_t B;
+    // Misc
+    uintptr_t MusicVolume;
+    uintptr_t SoundEffectVolume;
+    uintptr_t VoiceVolume;
+    uintptr_t AudioOutput;
+    uintptr_t VoiceChanger;
+    uintptr_t ScreenBrightness;
+    uintptr_t Distance;
+    uintptr_t CombatDistance;
+    uintptr_t VerticalRotationSpeed;
+    uintptr_t HorizontalRotationSpeed;
+    uintptr_t HorizontalAutoAdjust;
+    uintptr_t VerticalAutoAdjust;
+    uintptr_t FreeEnemyTracking;
+    uintptr_t ZoomSpeed;
+    uintptr_t PursuitSpeed;
+    uintptr_t LockedEnemyTracking;
+    uintptr_t HUDOpacity;
+    uintptr_t R;
+    uintptr_t G;
+    uintptr_t B;
 
-	//Cheats
-	cheat NoClipX;
-	cheat NoClipY;
-	cheat InfiniteDoubleJump;
-	cheat NoCooldown;
-	cheat InfiniteAirDash;
-	cheat WeaponMaterials;
-	cheat PodMaterials;
-	cheat FreeCamera;
-	cheat InfiniteItemUsage;
+    // Cheats
+    cheat NoClipX;
+    cheat NoClipY;
+    cheat InfiniteDoubleJump;
+    cheat NoCooldown;
+    cheat InfiniteAirDash;
+    cheat WeaponMaterials;
+    cheat PodMaterials;
+    cheat FreeCamera;
+    cheat InfiniteItemUsage;
 };
